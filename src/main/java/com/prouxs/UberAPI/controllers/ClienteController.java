b2e1224a48b5867d7751de18dfd911d3c905d7f7
Sulma Fabiola <49580355+SFMZ21@users.noreply.github.com>
2021-05-15 17:47:02 -0600

    Controller cliente realizado

---
+++ src/main/java/com/prouxs/UberAPI/controllers/ClienteController.java
@@ -0,0 +1,30 @@
+package com.prouxs.UberAPI.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import com.prouxs.UberAPI.entities.Cliente;
+import com.prouxs.UberAPI.repositories.ClienteRepository;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping(value ="/clientes")
+public class ClienteController {
+
+    @Autowired
+    ClienteRepository clienteRepository;
+
+//metodo crear clientes
+
+    @PostMapping
+    @ResponseStatus(code=HttpStatus.ACCEPTED)
+    public Cliente crearClientes(@RequestBody Cliente cliente, @DateTimeFormat(pattern="yyyy.MM.ddd") Date date){
+        Cliente nuevoCliente = clienteRepository.save(cliente);
+        return nuevoCliente;
+    }
+
+}
+
