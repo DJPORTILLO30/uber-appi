07ae9f477451b3ad2c370a90405f33daffe0fb10
Davinderpro01 <47469466+Davinderpro01@users.noreply.github.com>
2021-05-15 15:49:21 -0600

    Avance

---
+++ src/main/java/repositories/ViajeRepository.java
@@ -0,0 +1,7 @@
+package repositories;
+
+import com.prouxs.UberAPI.entities.Viaje;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ViajeRepository extends CrudRepository<Viaje, Long> {
+}
